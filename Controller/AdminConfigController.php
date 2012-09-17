782440962f3bba9587eaf255e0bf4fb07be681f1
Eddie Fisher <eddi.fisher@gmail.com>
2012-09-17 18:50:46 +0400

    add generate form

--- Controller/AdminConfigController.php
+++ Controller/AdminConfigController.php
@@ -9,6 +9,7 @@ use JMS\SecurityExtraBundle\Annotation\Secure;
 use Smirik\ConfigBundle\Model\Config;
 use Smirik\ConfigBundle\Model\ConfigQuery;
 use Smirik\ConfigBundle\Form\Type\ConfigType;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/admin")
@@ -20,13 +21,28 @@ class AdminConfigController extends Controller
    * @Secure(roles="ROLE_USER")
    * @Template("SmirikConfigBundle:Config:config.html.twig")
   */
-  public function adminAction()
+  public function adminAction(Request $request)
   {
+    $test = '';
     $configs = ConfigQuery::create()->find();
-    
+    if ($request->getMethod() == 'POST')
+    {
+      $configsData = $this->get('request')->request->get('config');
+      foreach ($configsData as $id => $value)
+      {
+        $config = ConfigQuery::create()->findPk($id);
+        if (!$config) {
+          throw $this->createNotFoundException('ConfigBundle: поле не найдено: '.$id);
+        }
+        $test .= $id . " - " . $value . "<br />";
+        $config->setValue($value);
+        $config->save();
+      }
+    }
 
     return array(
       'configs' => $configs,
+      'test'=>$test,
     );
   }
 }
