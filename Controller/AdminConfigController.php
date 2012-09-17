cebab02a826eb2c8d5556de922904d8370322a1a
Eddie Fisher <eddi.fisher@gmail.com>
2012-09-17 17:03:29 +0400

    add admin layer

--- Controller/AdminConfigController.php
+++ Controller/AdminConfigController.php
@@ -3,12 +3,30 @@
 namespace Smirik\ConfigBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use JMS\SecurityExtraBundle\Annotation\Secure;
+use Smirik\ConfigBundle\Model\Config;
+use Smirik\ConfigBundle\Model\ConfigQuery;
+use Smirik\ConfigBundle\Form\Type\ConfigType;
 
+/**
+ * @Route("/admin")
+ */
 class AdminConfigController extends Controller
 {
-	public function indexAction($name)
-	{
-		var_dump('stop');
-		exit();
-	}
+  /**
+   * @Route("/config", name="admin_config")
+   * @Secure(roles="ROLE_USER")
+   * @Template("SmirikConfigBundle:Config:config.html.twig")
+  */
+  public function adminAction()
+  {
+    $configs = ConfigQuery::create()->find();
+    
+
+    return array(
+      'configs' => $configs,
+    );
+  }
 }
