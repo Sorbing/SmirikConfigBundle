336f78f6a63da569990eb13455e46114b8e817f1
Evgeny Smirnov <smirik@gmail.com>
2012-09-20 14:12:29 +0400

    Add set method to config.

--- Config/Config.php
+++ Config/Config.php
@@ -3,6 +3,7 @@
 namespace Smirik\ConfigBundle\Config;
 
 use Smirik\ConfigBundle\Model\ConfigQuery;
+use Smirik\ConfigBundle\Model\Config as ModelConfig;
 
 class Config
 {
@@ -56,4 +57,69 @@ class Config
 		}
 		return false;
   }
+
+	/**
+	 * Set config value (or override)
+	 * @param string $key
+	 * @param string $value
+	 * @param boolean $create_if_not_exists
+	 * @return Smirik\ConfigBundle\Model\Config
+	 */
+	public function set($key, $value, $create_if_not_exists = true)
+	{
+		$keys = explode('.', $key);
+		
+		$first = ConfigQuery::create()
+			->filterByKey($keys[0])
+			->filterByPid(null)
+			->findOne();
+		
+		if (!$first)
+		{
+			if (!$create_if_not_exists)
+			{
+				return false;
+			}
+			$first = new ModelConfig();
+			$first->setKey($keys[0]);
+			if (count($keys) == 1)
+			{
+				$first->setValue($value);
+			} else
+			{
+				$first->setValue('');
+			}
+			$first->setPid(null);
+			$first->save();
+		}
+		
+		unset($keys[0]);
+		
+		foreach ($keys as $one)
+		{
+			$new = ConfigQuery::create()
+				->filterByKey($one)
+				->filterByPid($first->getId())
+				->findOne();
+			
+			if (!$new)
+			{
+				if (!$create_if_not_exists)
+				{
+					return false;
+				}
+				$new = new ModelConfig();
+				$new->setKey($one);
+				$new->setValue('');
+				$new->setPid($first->getId());
+				$new->save();
+			}
+			
+			$first = $new;
+		}
+		
+		$first->setValue($value);
+		$first->save();
+	}
+
 }
